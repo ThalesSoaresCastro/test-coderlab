896c317123eb03dd81fe3d41628b6c9cdc43c5fa
thalescs <th4les.castro@gmail.com>
2025-04-25 18:23:48 -0300

    feat: create category controller, module

---
+++ backend/src/category/category.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.category.findMany({
+      include: {
+        children: true,
+      },
+    });
+  }
+}
