3f28d7fee04117470a58893f5573f1a6280c4f1d
thalescs <th4les.castro@gmail.com>
2025-04-26 13:26:19 -0300

    feat: finalizando backend

---
+++ backend/src/env-validate.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { config } from 'dotenv';
+
+config();
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().min(1, "Database URL is required"),
+  PORT: z.string().optional().default("3000"),
+
+});
+
+function validateEnv() {
+  try {
+    envSchema.parse(process.env);
+    console.log("Environment variables are valid");
+  } catch (error) {
+    console.error("Error: Invalid environment variables:", error.errors);
+    process.exit(1);
+  }
+}
+
+export default validateEnv;
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory, NestApplication } from '@nestjs/core';
 import { AppModule } from './app.module';
+import validateEnv from './env-validate';
 
 async function bootstrap() {
+  validateEnv();
+  const port = process.env.PORT ?? 3000;
   const app = await NestFactory.create<NestApplication>(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port).then(()=>{
+    console.log(`API server listening on port ${port}`);
+  });
 }
 bootstrap();
